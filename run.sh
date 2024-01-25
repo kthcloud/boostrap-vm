aa242120e14164ae1dfbccd839af75853a450b97
saffronjam <own.emka@gmail.com>
2024-01-25 14:30:51 +0100

    update copy motd message

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ apt-get autoremove -y
 
 # Copy 99-kthcloud to /etc/motd.d
 echo "Copying 99-kthcloud to /etc/motd.d"
-cp 99-kthcloud /etc/motd.d
+cp 99-kthcloud /etc/update-motd.d/
+chmod +x /etc/update-motd.d/99-kthcloud
 
 apt install -y neofetch
